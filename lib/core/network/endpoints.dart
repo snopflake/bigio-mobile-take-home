0f4467bd2c71a0f65a4585e8558df63fa35a7a81
snopflake <nofanisrina@gmail.com>
2026-01-15 11:40:43 +0700

    chore: setup network - create endpoints

---
+++ lib/core/network/endpoints.dart
@@ -0,0 +1,14 @@
+
+class Endpoints {
+
+  Endpoints._();
+
+  // URL
+  static const String baseUrl = 'https://rickandmortyapi.com/api';
+
+  // Paths
+  static const String characters = '$baseUrl/character';
+  static const String locations = '$baseUrl/location';
+  static const String episodes = '$baseUrl/episode';
+
+}
