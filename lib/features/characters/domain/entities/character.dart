abb74f7e234cb518b517fd5e6f83603a2ee8fed0
snopflake <nofanisrina@gmail.com>
2026-01-15 22:40:27 +0700

    feat: add character entities on domain

---
+++ lib/features/characters/domain/entities/character.dart
@@ -0,0 +1,19 @@
+class Character {
+  final int id;
+  final String name;
+  final String species;
+  final String gender;
+  final String origin;
+  final String location;
+  final String image;
+
+  const Character({
+    required this.id,
+    required this.name,
+    required this.species,
+    required this.gender, 
+    required this.origin,
+    required this.location,
+    required this.image,
+  });
+}
