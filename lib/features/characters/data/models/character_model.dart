379d7857d38a594748409d7f698a1bd0a386a9ee
snopflake <nofanisrina@gmail.com>
2026-01-15 13:41:18 +0700

    feat: add character model

---
+++ lib/features/characters/data/models/character_model.dart
@@ -0,0 +1,31 @@
+class CharacterModel {
+  final int id;
+  final String name;
+  final String species;
+  final String gender;
+  final String origin;
+  final String location;
+  final String image;
+
+  const CharacterModel({
+    required this.id,
+    required this.name,
+    required this.species,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+  });
+
+  factory CharacterModel.fromJason(Map<String, dynamic> json) {
+    return CharacterModel(
+      id: json['id'] as int,
+      name: (json['name'] ?? '') as String,
+      species: (json['species'] ?? '') as String,
+      gender: (json['gender'] ?? '') as String,
+      origin: (json['origin']['name'] ?? '') as String,
+      location: (json['location']['name'] ?? '') as String,
+      image: (json['image'] ?? '') as String,
+    );
+  }
+}
