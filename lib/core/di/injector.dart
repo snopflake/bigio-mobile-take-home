32f6866442b10e5ae275e923c7728a0573f8ae49
snopflake <nofanisrina@gmail.com>
2026-01-16 01:09:18 +0700

    feat: add character usecases to di

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -8,6 +8,10 @@ import '../../features/characters/data/datasources/character_remote_data_source.
 import '../../features/characters/data/repositories/character_repository_impl.dart';
 import '../../features/characters/domain/repositories/character_repository.dart';
 
+import '../../features/characters/domain/usecases/get_characters.dart';
+import '../../features/characters/domain/usecases/get_character_detail.dart';
+import '../../features/characters/domain/usecases/search_characters.dart';
+
 final sl = GetIt.instance;
 
 Future<void> setupInjector() async {
@@ -29,5 +33,16 @@ void _registerCore() {
   // Character Repositories
   sl.registerLazySingleton<CharacterRepository>(
     () => CharacterRepositoryImpl(sl()),
+  );
+
+  // Character Use Cases
+  sl.registerLazySingleton<GetCharacters>(
+    () => GetCharacters(sl())
+  );
+  sl.registerLazySingleton<GetCharacterDetail>(
+    () => GetCharacterDetail(sl())
+  );
+  sl.registerLazySingleton<SearchCharacters>(
+    () => SearchCharacters(sl())
   );
 }
