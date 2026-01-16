5e19e410efbcb1a092e6134254299d795ffc69bc
snopflake <nofanisrina@gmail.com>
2026-01-16 08:38:54 +0700

    feat: add reusable appcolors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF3B82F6);
+  static const background = Color(0xFFF8FAFC);
+  static const surface = Colors.white;
+
+  static const textPrimary = Color(0xFF0F172A);
+  static const textSecondary = Color(0xFF64748B);
+
+  static const error = Color(0xFFEF4444);
+}
