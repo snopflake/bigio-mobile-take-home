d80acff4531f0023e959e6a896e84d29057abdeb
snopflake <nofanisrina@gmail.com>
2026-01-16 17:47:13 +0700

    feat: add reusable widget character card

---
+++ lib/features/characters/presentation/widgets/character_card.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theme/app_colors.dart';
+import '../../../../core/theme/app_text_styles.dart';
+import '../../domain/entities/character.dart';
+import '../bloc/favorites_sync/favorites_sync_cubit.dart';
+import '../bloc/favorites_sync/favorites_sync_state.dart';
+
+class CharacterCard extends StatelessWidget {
+  final Character character;
+  final VoidCallback onTap;
+
+  const CharacterCard({
+    super.key,
+    required this.character,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final favState = context.watch<FavoritesSyncCubit>().state;
+    final isFav = favState is FavoritesSyncLoaded && favState.isFav(character.id);
+
+    return InkWell(
+      borderRadius: BorderRadius.circular(14.r),
+      onTap: onTap,
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14.r),
+          side: BorderSide(color: Colors.grey.shade300, width: 1),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.w),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12.r),
+                    child: AspectRatio(
+                      aspectRatio: 1.05,
+                      child: Image.network(
+                        character.image,
+                        fit: BoxFit.cover,
+                        errorBuilder: (_, __, ___) => Container(
+                          color: Colors.black12,
+                          alignment: Alignment.center,
+                          child: const Icon(Icons.broken_image),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 6.h,
+                    right: 6.w,
+                    child: InkWell(
+                      onTap: () => context.read<FavoritesSyncCubit>().toggle(character),
+                      borderRadius: BorderRadius.circular(999),
+                      child: Container(
+                        padding: EdgeInsets.all(6.w),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withValues(alpha: 0.9),
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.grey.shade300),
+                        ),
+                        child: Icon(
+                          isFav ? Icons.favorite : Icons.favorite_border,
+                          color: isFav ? Colors.redAccent : AppColors.primary,
+                          size: 16.sp,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.h),
+              Flexible(
+                child: Text(
+                  character.name,
+                  style: AppTextStyles.body.copyWith(fontWeight: FontWeight.w700),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 2.h),
+              Flexible(
+                child: Text(
+                  '${character.species} - ${character.gender}',
+                  style: AppTextStyles.caption,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
