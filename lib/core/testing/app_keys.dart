0149f46fe5f0ff403e03c0cbbba5fb0fe313e0b2
snopflake <nofanisrina@gmail.com>
2026-01-16 12:34:12 +0700

    chore: add searchpage keys

--- lib/core/testing/app_keys.dart
+++ lib/core/testing/app_keys.dart
@@ -2,15 +2,21 @@ import 'package:flutter/widgets.dart';
 
 class AppKeys {
 
+  // Reusable
+  static Key characterCard(int id) => ValueKey('character_card_$id');
+
   // HomePage Keys
   static const homeSearchField = Key('home_search_field');
   static const homeFavoritesButton = Key('home_favorites_button');
-  static Key characterCard(int id) => ValueKey('character_card_$id');
 
   // DetailPage Keys
   static const detailFavoriteButton = Key('detail_favorite_button');
   static const detailImage = Key('detail_image');
   static const detailName = Key('detail_name');
   static Key detailInfoTile(String label) => ValueKey('detail_info_tile_$label');
+
+   // SearchPage Keys
+  static const searchField = Key('search_field');
+  static const searchClearButton = Key('search_clear_button');
 
 }
